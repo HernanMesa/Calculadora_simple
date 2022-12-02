c6bbe1bf93f83556c94c993a2fe0ec5ebf2ddd56
HernánMesa <toscosnake01@gmail.com>
2022-12-02 18:46:20 -0500

    Agregamos la historia 4

--- calculadora_simple.cs
+++ calculadora_simple.cs
@@ -15,7 +15,7 @@ namespace Calculadora
             int resultado;
             
 
-            Console.WriteLine("Para suma ingresa 1 , para resta 2, para multiplicación 3, para division 4");
+            Console.WriteLine("Para suma ingresa 1 , para resta 2, para multiplicación 3, para división 4");
 
            
 
@@ -58,20 +58,20 @@ namespace Calculadora
 
                         break;
 
-                        case 4:
+                    case 3:
 
-                    Console.WriteLine("Ingrese el primer número");
-                    num1 = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Ingrese el segundo número");
-                    num2 = int.Parse(Console.ReadLine());
-                    resultado = num1 / num2;
-                    Console.WriteLine($"El resultado es: {resultado}");
+                        Console.WriteLine("Ingrese el primer número");
+                        num1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo número");
+                        num2 = int.Parse(Console.ReadLine());
+                        resultado = num1 / num2;
+                        Console.WriteLine($"El resultado es: {resultado}");
 
-                    break;
+                        break;
 
 
                     default:
-                        Console.WriteLine("Debe ingresar números del 1 al 3");
+                        Console.WriteLine("Debe ingresar números del 1 al 4");
                         break;
                 }
                 }
